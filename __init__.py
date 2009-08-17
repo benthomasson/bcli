b932bd44d93c92e67acb9c0ba8eedbc4b6b6f333
Ben Thomasson <ben.thomasson@gmail.com>
2009-08-17 17:32:48 -0400

    Adding command factory to generate simple unix commands

--- __init__.py
+++ __init__.py
@@ -1,7 +1,33 @@
 
-import glob
+from subprocess import Popen, PIPE, STDOUT
 
-def ls():
-    for x in glob.glob('*'): 
-        print x
-    
+def command_factory(x):
+    def command(args="",printOutput=True):
+        output = Popen(  x.__name__ + " " + args,
+                shell=True,
+                stdout=PIPE,
+                stderr=STDOUT).communicate()[0]
+        if printOutput:
+            print output
+            return None
+        else:
+            return output
+    return command
+
+@command_factory
+def ls(): pass
+
+@command_factory
+def grep(): pass
+
+@command_factory
+def cat(): pass
+
+@command_factory
+def pwd(): pass
+
+@command_factory
+def df(): pass
+
+@command_factory
+def du(): pass
