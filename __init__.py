c43c26714e59defa95f8981be4c15335566a6e6b
Ben Thomasson <ben.thomasson@gmail.com>
2009-10-16 17:13:59 -0400

    Fixing argument passing for bcli commands

--- __init__.py
+++ __init__.py
@@ -1,9 +1,20 @@
 
+__all__ = ['ls',
+           'grep',
+           'cat',
+           'pwd',
+           'df',
+           'du',]
+
 from subprocess import Popen, PIPE, STDOUT
 
 def command_factory(x):
-    def command(args="",printOutput=True):
-        output = Popen(  x.__name__ + " " + args,
+    def command(*args,**kwargs):
+        printOutput = True
+        if 'printOutput' in kwargs:
+            printOutput = kwargs['printOutput']
+        cmd =  x.__name__ + " " + " ".join(map(str,args))
+        output = Popen( cmd,
                 shell=True,
                 stdout=PIPE,
                 stderr=STDOUT).communicate()[0]
